7db5daca95315b6076ae470cdbc1202464c7aadd
johnmuthua <caveomuthama@gmail.com>
2021-01-23 23:33:23 +0300

    created header component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { GlobalStyles } from "./global-styles";
+import "normalize.css";
 import { render } from "react-dom";
 import App from "./app";
 
-render(<App />, document.getElementById("root"));
+render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
