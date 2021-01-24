6d90e7e86548ae3520e482fe8edfcf72c50f4892
johnmuthua <caveomuthama@gmail.com>
2021-01-24 15:00:56 +0300

    feature:created pages home, signup, in, and browse

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faq";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constatnts/routes";
+import { Home, Browse, Signin, Signup } from "./pages";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
