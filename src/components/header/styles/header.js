813f6858340c80524b689c1b3860056f5578b838
johnmuthua <caveomuthama@gmail.com>
2021-01-25 12:13:00 +0300

    Fix: fixed an eslint bug

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Background = styled.div``;
+
+export const Frame = styled.div``;
+
+export const Logo = styled.img``;
+
+export const Container = styled.div``;
--- src/containers/header.js
+++ src/containers/header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header } from "../components";
+
 export default function HeaderContainer() {
   <Header>
     <Header.Frame>
