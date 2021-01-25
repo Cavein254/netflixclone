fb6e7ded8b5642258f7886a8f42d1b853bcc2e58
johnmuthua <caveomuthama@gmail.com>
2021-01-25 09:16:03 +0300

    feature: Added a header component

---
+++ src/containers/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../components";
+export default function HeaderContainer() {
+  <Header>
+    <Header.Frame>
+      <Header.Logo />
+      <Header.Button />
+    </Header.Frame>
+  </Header>;
+}
