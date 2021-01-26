2b7eafc91a2fdf55c6ea567af0186c0af036e7f9
johnmuthua <caveomuthama@gmail.com>
2021-01-26 11:44:46 +0300

    feat: created a signup page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function SignIn() {
-  return <h1>Sign In</h1>;
+  console.log(Form);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+
+  const isInValid = password === "" || email === "";
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInValid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to NetFlix? <Form.Link to="/signup">Sign Up Now</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by the Google reCAPTCHA to ensure you're not
+            a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
