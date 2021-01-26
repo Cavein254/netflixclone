b03fcbc5fa062ef831e18c3e16d1695e6c2a7725
johnmuthua <caveomuthama@gmail.com>
2021-01-26 10:27:18 +0300

    chore: added seeder to seed  firebase with data

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,9 +1,12 @@
 import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import { seedDatabase } from "../seed";
 
 //Seed database
 const config = {};
 const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
 
 export { firebase };
