b54be63eafb47da2c4eb325b71652cb4e2409a34
johnmuthua <caveomuthama@gmail.com>
2021-01-26 10:07:24 +0300

    feat: Added firebase functioality

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,9 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//Seed database
+const config = {};
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
