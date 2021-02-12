43d5a2706072a5e5a3cf6a33ebbc7d7336285a2b
johnmuthua <caveomuthama@gmail.com>
2021-02-12 17:20:18 +0300

    chore: updated the landing page

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,7 +4,14 @@ import "firebase/auth";
 import { seedDatabase } from "../seed";
 
 //Seed database
-const config = {};
+const config = {
+  apiKey: "AIzaSyBihCiDcfG0S5Ld98rMxyJY9tCq-jG_KnQ",
+  authDomain: "netflix-4c350.firebaseapp.com",
+  projectId: "netflix-4c350",
+  storageBucket: "netflix-4c350.appspot.com",
+  messagingSenderId: "1055561272501",
+  appId: "1:1055561272501:web:9abe68a458dcbddecd02fc",
+};
 const firebase = Firebase.initializeApp(config);
 
 // seedDatabase(firebase);
